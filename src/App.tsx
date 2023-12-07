235ad89fef4232a05e8277ef7793012849a9ad1f
Clara Lecroisey <clalecroisey@gmail.com>
2023-12-07 18:48:44 +0100

    feat: add Google Maps foundation using gmaps library

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
+import { Status, Wrapper } from "@googlemaps/react-wrapper";
 import "./App.css";
 
+const render = (status: Status) => {
+  return <span>{status}</span>;
+};
+
 function App() {
-  return <>Hello</>;
+  return (
+    <Wrapper apiKey={import.meta.env.GOOGLE_API_KEY} render={render}>
+      <div id="map">Map here</div>
+    </Wrapper>
+  );
 }
 
 export default App;
