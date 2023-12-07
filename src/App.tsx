f1b0ce0791b1107dfde2e4577965aaf4ad1de8e4
Clara Lecroisey <clalecroisey@gmail.com>
2023-12-07 20:12:01 +0100

    feat: create Map component and display empty map

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Status, Wrapper } from "@googlemaps/react-wrapper";
 import "./App.css";
+import { Map } from "./components/Map";
 
 const render = (status: Status) => {
   return <span>{status}</span>;
@@ -8,7 +9,14 @@ const render = (status: Status) => {
 function App() {
   return (
     <Wrapper apiKey={import.meta.env.GOOGLE_API_KEY} render={render}>
-      <div id="map">Map here</div>
+      <h1>Tokyo restaurants</h1>
+      <Map
+        // TODO: Temporary styles, to change later
+        style={{
+          width: "80vw",
+          height: "80vh",
+        }}
+      />
     </Wrapper>
   );
 }
---
+++ src/components/Map.tsx
@@ -0,0 +1,24 @@
+import { useEffect, useRef, useState } from "react";
+
+interface MapProps extends google.maps.MapOptions {
+  style: { [key: string]: string };
+  onClick?: (e: google.maps.MapMouseEvent) => void;
+  onIdle?: (map: google.maps.Map) => void;
+}
+
+export function Map({ onClick, onIdle, style }: MapProps) {
+  const ref = useRef<HTMLDivElement>(null);
+  const [map, setMap] = useState<google.maps.Map>();
+
+  useEffect(() => {
+    if (ref.current && !map) {
+      setMap(new window.google.maps.Map(ref.current, {}));
+    }
+  }, [ref, map]);
+
+  return (
+    <>
+      <div ref={ref} style={style} />
+    </>
+  );
+}
